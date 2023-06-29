bd56fd6a5ca8df46de0610cbe17a71d36e0c569f
furacas <s.furacas@outlook.com>
2023-06-29 14:59:02 +0800

    chore: test publish

--- setup.py
+++ setup.py
@@ -2,20 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="star_rail_gps",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A brief description of your package",
+    author="furacas",
+    author_email="s.furacas@outlook.com",
+    description="Honkai: Star Rail GPS",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_package",
+    url="https://github.com/furacas/StarRailGPS",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     package_data={
         'star_rail_gps': ['resources/*'],
     },
