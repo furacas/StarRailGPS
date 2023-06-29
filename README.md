cb607cb640ac1daa5ec7f7a7a0001de4cf9700c1
furacas <s.furacas@outlook.com>
2023-06-29 19:07:49 +0800

    feat: position

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 崩坏：星穹铁道GPS
+# StarRailGPS
+
+`StarRailGPS`是一个 Python 包，它提供了一个函数，用于在游戏中找到小地图在大地图中的位置。
--- star_rail_gps/scales.py
+++ star_rail_gps/scales.py
@@ -2,35 +2,16 @@
 import cv2
 import numpy as np
 
+from star_rail_gps.gps import get_mask_from_gray_map, get_mask_from_rgb_min_map
 from star_rail_gps.utils.resources import resource_path
 
-
-def get_mask_from_rgb(img_r):
-    img_hsv = cv2.cvtColor(img_r, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(img_hsv)
-    # 筛选白色 H S<10  V 60~90%
-    mask1 = (s < 25) * (v > 255 * 0.6) * (v < 255 * 0.9)
-    # 筛选蓝色摄像头扫过的白色
-    mask2 = (95 < h) * (h < 105) * (0 < s) * (s < 50) * (200 < v) * (v < 240)
-    mask = mask1 | mask2
-    img_mask = mask.astype(np.uint8) * 255
-    return img_mask
-
-
-def get_mask_from_gray_image(map_bgra):
-    b, g, r, a = cv2.split(map_bgra)
-    gray = b
-    mask = (a > 250) & (gray > 80)
-    return mask.astype(np.uint8) * 255
-
-
 img = cv2.imread(resource_path('maps/50.png'), cv2.IMREAD_UNCHANGED)
-img = get_mask_from_gray_image(img)
+img = get_mask_from_gray_map(img)
 
 screen = cv2.imread(resource_path('test_data/screen_1920_1080.png'))
 minimap_rect = [77, 88, 127, 127]  # (x, y, width, height)
 template = screen[minimap_rect[1]:minimap_rect[1] + minimap_rect[3], minimap_rect[0]:minimap_rect[0] + minimap_rect[2]]
-template = get_mask_from_rgb(template)
+template = get_mask_from_rgb_min_map(template)
 
 h, w = template.shape[::]
 # 创建尺度的列表
